bfafa7f3def94db147183f3a37de53664e3b9cb4
dinushchathurya <dinushchathurya21@gmail.com>
2021-04-12 20:59:46 +0530

    feat: create left_nav.blade.php file

---
+++ resources/views/layouts/inc/left_nav.blade.php
@@ -0,0 +1,16 @@
+<div class="br-logo"><a href=""><span>[</span>CodingtrickS<span>]</span></a></div>
+<div class="br-sideleft overflow-y-auto">
+    <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
+    <div class="br-sideleft-menu">
+        <a href="" class="br-menu-link">
+            <div class="br-menu-item">
+                <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
+                <span class="menu-item-label">Management</span>
+                <i class="menu-item-arrow fa fa-angle-down"></i>
+            </div>
+        </a>
+        <ul class="br-menu-sub nav flex-column">
+            <li @if (\Request::is()) class='active' @endif class="nav-item"><a href="" class="nav-link">Nav Link</a></li>
+        </ul>
+    </div>
+</div>
