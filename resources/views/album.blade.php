ddb8b7e32e3b80147285b4300f5ef15f2c25d8fd
dinushchathurya <dinushchathurya21@gmail.com>
2021-04-15 10:01:58 +0530

    refactor: intergrade new template with album.blade.php

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -1,82 +1,41 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>{{$album->name}}</title>
-    <!-- Latest compiled and minified CSS -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-      body {
-        padding-top: 50px;
-      }
-      .starter-template {
-        padding: 40px 15px;
-        text-align: center;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <button type="button" class="navbar-toggle"data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">Awesome Albums</a>
-        <div class="nav-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="{{route('create_album_form')}}">Create New Album</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-     </div>
-    </div>
-    <div class="container">
+@extends('layouts.app')
 
-      <div class="starter-template">
-        <div class="media">
-          <img class="media-object pull-left"alt="{{$album->name}}" src="/albums/{{$album->cover_image}}" width="350px">
-          <div class="media-body">
-            <h2 class="media-heading" style="font-size: 26px;">Album Name:</h2>
-            <p>{{$album->name}}</p>
-          <div class="media">
-          <h2 class="media-heading" style="font-size: 26px;">AlbumDescription :</h2>
-          <p>{{$album->description}}<p>
-          <a href="{{route('add_image',array('id'=>$album->id))}}"><button type="button"class="btn btn-primary btn-large">Add New Image to Album</button></a>
-          <a href="{{route('delete_album',array('id'=>$album->id))}}" onclick="return confirm('Are yousure?')"><button type="button"class="btn btn-danger btn-large">Delete Album</button></a>
-        </div>
+@section('content')
+
+  <div class="card shadow-base bd-0 rounded-0 widget-4">
+      <div class="card-header ht-75" style="background-color:#17A2B8;">
       </div>
-    </div>
-    </div>
-    <div class="row">
-        @foreach($album->Photos as $photo)
-          <div class="col-lg-3">
-            <div class="thumbnail" style="max-height: 350px;min-height: 350px;">
-              <img alt="{{$album->name}}" src="/albums/{{$photo->image}}">
-              <div class="caption">
-                <p>{{$photo->description}}</p>
-                <p>Created date:  {{ date("d F Y",strtotime($photo->created_at)) }}at {{ date("g:ha",strtotime($photo->created_at)) }}</p>
-                <a href="{{URL::route('delete_image',array('id'=>$photo->id))}}" onclick="returnconfirm('Are you sure?')"><button type="button"class="btn btn-danger btn-small">Delete Image</button></a>
-                <p>Move image to another Album :</p>
-                <form name="movephoto" method="POST"action="{{URL::route('move_image')}}">
-                    {{ csrf_field() }}
-                  <select name="new_album">
-                    @foreach($albums as $others)
-                      {{-- <option value="{{$others->id}}">{{$others->name}}</option> --}}
-                    @endforeach
-                  </select>
-                  <input type="hidden" name="photo"value="{{$photo->id}}" />
-                  <button type="submit" class="btn btn-smallbtn-info" onclick="return confirm('Are you sure?')">Move Image</button>
-                </form>
-              </div>
-            </div>
+      <div class="card-body" style="background-color:#2e415b;">
+          <div class="card-profile-img">
+              <img src="/albums/{{$album->cover_image}}" alt="" style="width:100px;height:100px;">
           </div>
-      @endforeach
-    </div>
-    </div>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script-->
-  </body>
-</html>
+          <h4 class="tx-normal tx-roboto tx-white">{{$album->name}}</h4>
+          <p class="mg-b-25">{{$album->description}}</p>
+
+          <p class="mg-b-0 tx-24">
+            <a href="{{route('add_image',array('id'=>$album->id))}}"><button type="button" class="btn btn-primary btn-large">Add New Image to Album</button></a>
+            <a href="{{route('delete_album',array('id'=>$album->id))}}" onclick="return confirm('Are yousure?')"><button type="button" class="btn btn-danger btn-large">Delete Album</button></a>
+          </p>
+      </div><!-- card-body -->
+  </div>
+  
+   <div class="row mt-5">
+       @foreach($album->Photos as $photo)
+       <div class="col-lg-3">
+           <div class="col-md mg-t-20 mg-md-t-0">
+               <div class="card bd-0">
+                   <img class="card-img-top img-fluid" src="/albums/{{$photo->image}}" alt="Image">
+                   <div class="card-body rounded-bottom bg-success">
+                       <p class="card-text tx-white">
+                           <h6 class="card-text tx-white">{{$photo->description}}</h6>
+                           <h6 class="card-text tx-white">{{ date("d F Y",strtotime($photo->created_at)) }}</h6>
+                           <a href="{{URL::route('delete_image',array('id'=>$photo->id))}}" onclick="returnconfirm('Are you sure?')"><button type="button" class="btn btn-danger btn-small center">Delete Image</button></a>
+                       </p>
+                   </div>
+               </div>
+           </div>
+       </div>
+       @endforeach
+   </div>
+
+@endsection
